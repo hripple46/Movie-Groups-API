f4fce62f8be88a998f4a4f665cbe9c8705f702e5
Henry Ripple <hripple46@gmail.com>
2023-06-19 13:53:29 -0500

    configured api connection

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/api/test", function (req, res, next) {
+  res.json({ message: "hooray! Henry's very first api!" });
 });
 
 module.exports = router;
