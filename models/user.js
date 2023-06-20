315921ceff7ce06c71f12854c1a2d13cf8ec5079
Henry Ripple <hripple46@gmail.com>
2023-06-20 16:04:07 -0500

    user sign up saves user to db

---
+++ models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: { type: String, required: true, max: 100 },
+  password: { type: String, required: true },
+  groups: [{ type: Schema.Types.ObjectId, ref: "Group" }],
+  posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
+});
+
+module.exports = mongoose.model("User", UserSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const MessageSchema = require("../models/message.js");
+const User = require("../models/user.js");
 
 /* GET home page. */
 router.get("/api/message", async function (req, res, next) {
@@ -17,6 +18,15 @@ router.post("/api/message", async function (req, res, next) {
     text: req.body.text,
   });
   await message.save();
+  res.status(201).send();
+});
+router.post("/api/users/signup", async function (req, res, next) {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+  await user.save();
+  res.status(201).send();
 });
 
 module.exports = router;
